5695546b50a891e6475228a7b7d5cca4284a4141
MShefer <marina32812@gmail.com>
2024-12-24 17:59:25 +0200

    22 задание

---
+++ ЕГКР-21-12-24/Task-26.py
@@ -0,0 +1,23 @@
+with open('input.txt') as file:
+    N = int(file.readline())
+    product = {}
+    for i in file:
+        article, price, status = map(int, i.split())
+        if article not in product:
+            product[article] = [price, 1, status]
+        else:
+            product[article][1] += 1
+            product[article][2] += status
+
+sred = 0
+for i in product.values():
+    summ += i[0]
+
+sred = summ / len(product)
+
+ans= []
+for i in product.values():
+    if i[0] > sred:
+        ans.append(i)
+
+ans = sorted(ans, key=lambda x: (x[2], x[0], ))
