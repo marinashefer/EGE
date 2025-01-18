885134ed9c56fc3c54afb41ad755f7d9abaa60c0
MShefer <marina32812@gmail.com>
2025-01-18 15:54:44 +0200

    26 решено!

---
+++ 18-01-25/Task-26(№4660).py
@@ -0,0 +1,11 @@
+with open('26_4660.txt') as file:
+    N = int(file.readline())
+    prices = [int(i) for i in file]
+
+prices = sorted(prices, reverse=True)
+
+every_4 = sum([prices[i] for i in range(3, len(prices), 4)])
+client_price = sum(prices) - every_4 + every_4 / 2
+store_price = sum(prices[:3 * len(prices) // 4]) + sum(prices[3 * len(prices) // 4:])/2
+
+print(client_price, store_price)
