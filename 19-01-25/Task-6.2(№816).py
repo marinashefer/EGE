52b878e8320bcaa4941358cbe7a0127cde520971
marinashefer <78643165+marinashefer@users.noreply.github.com>
2025-01-21 00:14:23 +0300

    а нет, все поняла!)

--- 19-01-25/Task-6.2(№816).py
+++ 19-01-25/Task-6.2(№816).py
@@ -1,7 +1,18 @@
-print('x y z')
+# print('x y z')
+#
+# for x in range(0, 2):
+#     for y in range(0, 2):
+#         for z in range(0, 2):
+#             f = (not (x == (y <= z)))
+#             print(x, y, z, f)
 
-for x in range(0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            f = (not (x == (y <= z)))
-            print(x, y, z, f)
+from itertools import permutations
+
+def f(x, y, z):
+    return (not(x == (y <= z)))
+
+table =[(0, 0, 1), (0, 1, 1)]
+for p in permutations('xyz'):
+    u = [f(**dict(zip(p, t))) for t in table] == [1, 0]
+    if u:
+        print(*p)
--- 19-01-25/Task-7.2(№960).py
+++ 19-01-25/Task-7.2(№960).py
@@ -1,7 +1,18 @@
-print('a b c')
+# print('a b c')
+#
+# for a in range(0, 2):
+#     for b in range(0, 2):
+#         for c in range(0, 2):
+#             f = (a and (not b)) or c
+#             print(a, b, c, f)
 
-for a in range(0, 2):
-    for b in range(0, 2):
-        for c in range(0, 2):
-            f = (a and (not b)) or c
-            print(a, b, c, f)
+from itertools import permutations
+
+def f(a, b, c):
+    return (a and (not b)) or c
+
+table =[(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1, )]
+for p in permutations('abc'):
+    u = [f(**dict(zip(p, t))) for t in table] == [0, 1, 1, 1, 0, 0, 1, 1]
+    if u:
+        print(*p)
