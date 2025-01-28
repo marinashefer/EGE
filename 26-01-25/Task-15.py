93e87fef39fd864011f817cdba61fd5bc157a4f3
MShefer <marina32812@gmail.com>
2025-01-28 16:18:57 +0200

    решаем 26 тип

--- 26-01-25/Task-15.py
+++ 26-01-25/Task-15.py
@@ -1,14 +1,12 @@
 def f(A):
-    for x in range(0, 10000):
-        for y in range(0, 10000):
+    for x in range(1, 1000):
+        for y in range(1, 10000):
             f = (not((x * y) > A+13)) <= (((28 * y) > 520) or ((x * 25) > 800))
             if not f:
                 return False
     return True
 
-ans = []
-for A in range(0, 100000):
-    if f(A):
-        ans.append(A)
 
-print(max(ans))
+for A in range(-30, 100):
+    if f(A):
+        print(A)
