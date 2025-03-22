6a152c89ba3f62fc5f7a354e0757206037934791
marinashefer <78643165+marinashefer@users.noreply.github.com>
2025-03-22 10:10:05 +0300

    26 дз пробую

---
+++ 21-03-25 (дз 26 тип)/Task - 26(2480).py
@@ -0,0 +1,14 @@
+with open ('26.txt') as file:
+    n = int(file.readline())
+    roads = [list(map(int, i.split())) for i in file]
+
+roads = sorted(roads)
+
+ans = []
+for i in range(0, len(roads) - 1):
+    if roads[i+1][0] <= roads[i][1]:
+        ans.append(max(roads[i + 1][1], roads[i][1]) -  roads[i][0])
+        roads[i] = [roads[i][0], max(roads[i+1][1], roads[i][1])]
+
+print(len(ans), max(ans))
+print(ans)
