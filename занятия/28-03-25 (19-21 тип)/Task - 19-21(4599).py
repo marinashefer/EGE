5dc7f499a16701f1f62befd36741ed84690b19aa
marinashefer <78643165+marinashefer@users.noreply.github.com>
2025-04-01 02:02:04 +0300

    19-21 задача на две кучи

---
+++ занятия/28-03-25 (19-21 тип)/Task - 19-21(4599).py
@@ -0,0 +1,23 @@
+def f(x, y, s):
+    if x + y >= 259: return s % 2 == 0
+    if s == 0: return False
+    h = [f(x+1, y, s-1), f(x, y+1, s-1), f(x*2, y, s-1), f(x, y*2, s-1)]
+    return any(h) if (s-1) % 2 == 0 else all(h)
+
+print('19)', [s for s in range(1, 242) if f(17, s, 2)]) # для №19 5 строка: return any(h)
+print('20)', [s for s in range(1, 242) if f(17, s, 3) and not f(17, s, 1)])
+print('21)', [s for s in range(1, 242) if f(17, s, 4) and not f(17, s, 2)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
